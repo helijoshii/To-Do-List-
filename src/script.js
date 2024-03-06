71796bd99629bbc035c93b329f7582fd957e9c31
Heli Joshi <helijoshi45@gmail.com>
2024-03-06 17:23:10 +0530

    added checkbox

--- src/script.js
+++ src/script.js
@@ -1,56 +1,64 @@
-const inputBox = document.getElementById("input-box");                     // input-box is the id of the input box
-const listContainer = document.getElementById("list-container");           // list-container is the id of the ul
-const button = document.getElementById("add-task-btn");                    // add-task-btn is the id of the button
+const inputBox = document.getElementById("input-box"); // input-box is the id of the input box
+const listContainer = document.getElementById("list-container"); // list-container is the id of the ul
+const button = document.getElementById("add-task-btn"); // add-task-btn is the id of the button
 
-function addTask() {                                                      // function to add task
-  if (inputBox.value === "") {                                              
+function addTask() {
+  // function to add task
+  if (inputBox.value === "") {
     alert("Enter task");
   } else {
-    let li = document.createElement("li");                              // creating li element
-    li.classList.add("two");                                         // adding class to the li element           
-    li.innerHTML = inputBox.value;                            // adding the value of the input box to the li element
-    listContainer.appendChild(li);                                  // appending the li element to the ul
+    let checkbox = document.createElement("input");
+    checkbox.classList.add("three");
+    checkbox.type = "checkbox";
+    checkbox.id = "myCheckbox";
+    let li = document.createElement("li"); // creating li element
+    li.classList.add("two"); // adding class to the li element
+    li.innerHTML = inputBox.value; // adding the value of the input box to the li element
+    listContainer.appendChild(li); // appending the li element to the ul
     let span = document.createElement("span");
     span.classList.add("one");
     span.innerHTML = "\u00d7";
     li.appendChild(span);
+    li.insertBefore(checkbox, li.firstChild);
   }
-  inputBox.value = "";                                          
-  saveData();                                                       // calling the saveData function
+  inputBox.value = "";
+  saveData(); // calling the saveData function
 }
 
-// listContainer.addEventListener(
-//   "click",
-//   function (e) {
-//     if (e.target.tagName === "LI") {
-//       e.target.classList.toggle("checked");
-//     } else if (e.target.tagName === "SPAN") {
-//       e.target.parentElement.remove();
-//       saveData();
-//     }
-//   },
-//   false
-// );
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
 
 const saveData = () => {
-  localStorage.setItem("data", listContainer.innerHTML);            // saving the data to the local storage
+  localStorage.setItem("data", listContainer.innerHTML); // saving the data to the local storage
 };
 
 const showTask = () => {
-  listContainer.innerHTML = localStorage.getItem("data");           // displaying the data from the local storage
+  listContainer.innerHTML = localStorage.getItem("data"); // displaying the data from the local storage
 };
 
 inputBox.addEventListener("input", (e) => {
-  e.target.value = e.target.value.trim();                           // removing the white spaces from the input
-  if (!!e.target.value) {                                           // !! converts the value to boolean
-    button.classList.add("!bg-[#8a7862]");
+  e.target.value = e.target.value.trim(); // removing the white spaces from the input
+  if (!!e.target.value) {
+    // !! converts the value to boolean
+    button.classList.add("!bg-[#e675d0]");
   } else {
-    button.classList.remove("!bg-[#8a7862]");
+    button.classList.remove("!bg-[#e675d0]");
   }
 });
 
 inputBox.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {                                          // if the enter key is pressed
+  if (e.key === "Enter") {
+    // if the enter key is pressed
     addTask();
   }
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,11 @@ module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
     extend: {
-    backgroundImage:
-      {
-        'toDoBg': "url('ToDoBg.jpg')"
-        
-      }
+      backgroundImage: {
+        // 'toDoBg': "url('ToDoBg.jpg')"
+        toDoBg: "url('../src/images/bg-image.jpg')",
+      },
     },
   },
   plugins: [],
-}
-
+};
